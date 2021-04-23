366e8701e4553fd4b57ce5d8bbc46ac6bb0e32cb
Francis Adewale <71708756+FrancisAdewale@users.noreply.github.com>
2021-04-23 16:48:37 +0100

    add appropriate brand colour scheme

--- CognizantAssignment/CognizantAssignment/Controller/HomeViewController.swift
+++ CognizantAssignment/CognizantAssignment/Controller/HomeViewController.swift
@@ -16,38 +16,44 @@ class HomeViewController: UIViewController {
         super.viewWillAppear(animated)
         setUpRegisterButton()
         setUpLoginButton()
-     
+       
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
     }
     
     fileprivate func setUpRegisterButton() {
         
-        registerButton.setTitle("Register", for: .normal)
+        registerButton.setTitle("REGISTER", for: .normal)
         registerButton.frame = CGRect(x: view.center.x, y: view.center.y, width: 180, height: 80)
         registerButton.clipsToBounds = true
+        registerButton.titleLabel?.font = UIFont(name: "Arial", size: 30)
+        registerButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        registerButton.setTitleColor(.white, for: .normal)
         registerButton.layer.cornerRadius = 10.0
-        registerButton.layer.borderColor = UIColor.white.cgColor
+        registerButton.layer.borderColor = UIColor(red: 91.0, green: 155.0, blue: 213.0, alpha: 1.0).cgColor
         registerButton.layer.borderWidth = 2.0
         registerButton.center = CGPoint(x: view.center.x, y: view.center.y - 50)
-        registerButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         view.addSubview(registerButton)
     }
     
     
     fileprivate func setUpLoginButton() {
         
-        loginButton.setTitle("Login", for: .normal)
+        loginButton.setTitle("LOGIN", for: .normal)
         loginButton.frame = CGRect(x: view.center.x, y: view.center.y, width: 180, height: 80)
+        loginButton.titleLabel?.font = UIFont(name: "Arial", size: 30)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        loginButton.setTitleColor(.white, for: .normal)
         loginButton.clipsToBounds = true
         loginButton.layer.cornerRadius = 10.0
-        loginButton.layer.borderColor = UIColor.white.cgColor
+        loginButton.layer.borderColor = UIColor(red: 91.0, green: 155.0, blue: 213.0, alpha: 1.0).cgColor
         loginButton.layer.borderWidth = 2.0
         loginButton.center = CGPoint(x: view.center.x, y: view.center.y + 50)
-        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         view.addSubview(loginButton)
     }
     
